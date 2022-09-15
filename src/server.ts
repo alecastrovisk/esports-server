d748475507f0e4c4a34da9e6e2fedf6fded23c1f
Alexandre Castro <alexandre.acrsf@gmail.com>
2022-09-15 11:06:08 -0300

    Prisma ORM stuffs

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,25 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const app = express();
 
-app.get('/ads' , (request, response) => {
+
+app.get('/games', (request, response) => {
+  return response.json([]);
+});
+
+app.post('/ads' , (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads' , (request, response) => {
+  // const gameId = request.params.id;
   return response.json([
     {id: 1, name: 'anúncio 1'},
-])
+  ]);
+});
+
+app.get('/ads/:id/discord', (request, response) => {
+  return response.send({});
 })
 
 app.listen(3333)
