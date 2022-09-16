64a8924effb9cac83eaff5a5b103315dafdc7ff5
Alexandre Castro <alexandre.acrsf@gmail.com>
2022-09-16 09:31:40 -0300

    Db relation

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,38 @@
 import express, { request, response } from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
+app.use(express.json());
 
+const prisma = new PrismaClient();
 
-app.get('/games', (request, response) => {
-  return response.json([]);
+app.get('/games', async (request, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true
+        }
+      }
+    }
+  });
+
+  return response.json(games);
 });
 
 app.post('/ads' , (request, response) => {
   return response.status(201).json([]);
 });
 
-app.get('/games/:id/ads' , (request, response) => {
-  // const gameId = request.params.id;
-  return response.json([
-    {id: 1, name: 'anúncio 1'},
-  ]);
+app.get('/games/:id/ads' , async (request, response) => {
+  const gameId = request.params.id;
+
+  const ads = await prisma.ad.findMany({
+    where: {
+      gameId
+    }
+  })
+  return response.json(ads);
 });
 
 app.get('/ads/:id/discord', (request, response) => {
